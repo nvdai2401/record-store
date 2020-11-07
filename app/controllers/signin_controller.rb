a9f050546ab731ce96f5db152370cb2c0f856d61
nvdai2401 <nvdai2401@gmail.com>
2020-11-07 16:54:15 +0700

    develop signup and signin controllers

---
+++ app/controllers/signin_controller.rb
@@ -0,0 +1,29 @@
+class SigninController < ApplicationController
+  before_action : :authorized_access_request!, only: [:destroy]
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.authenticate(params[:password])
+      payload = { user_id: user.id }
+      session = JWTSessions::Session..new(payload: payload, refresh_by_access_allowed: true)
+      tokens = session.login
+      respone.set_cookie(JWTSessions.access_cookie, value: tokens[:access], httponly: true, secure: Rails.env.production?)
+
+      render json: { crsf: tokens[:crsf] }
+    else
+      not_authorized
+    end
+  end
+
+  def destroy
+    session = JWTSessions::Session.new(payload: payload)
+    session.flush_by_by_access_payload
+    render json: :ok
+  end
+
+  private
+    def not_found
+      render json: { error: 'Can not find email/password combination' }, status: :not_found
+    end
+end
